72d425c4f236a6430be60e0907c10593cc7b8d0f
anjamarjanovic <marjanovic.anja@outlook.com>
2022-02-26 19:43:37 +0100

    Add custome Directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-
+  @Output() clickedPage = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(feature:string){
+      this.clickedPage.emit(feature)
+  }
 }
